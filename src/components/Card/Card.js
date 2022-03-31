7851a02d57fe25c8e1a08f036bf67a80dc8eb0c5
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-03-31 13:04:07 +0200

    Add removeBook

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,31 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
   const cardId = props.cardId;
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
+  const handleFavorite = e => {
     e.preventDefault();
     dispatch(toggleCardFavorite(cardId));
+  }
+
+  const handleRemove = e => {
+    e.preventDefault();
+    dispatch(removeCard(cardId));
   }
 
   return (
     <li className={styles.card}>
       {props.title}
-      <form onSubmit={handleSubmit}>
-        <button>
+      <form>
+        <button onClick={handleFavorite}>
           <span className={clsx('fa fa-star-o', props.isFavorite && styles.isFavorite)}></span>
+        </button>
+        <button  onClick={handleRemove}>
+          <span className={clsx('fa fa-trash')}></span>
         </button>
       </form>  
     </li>
